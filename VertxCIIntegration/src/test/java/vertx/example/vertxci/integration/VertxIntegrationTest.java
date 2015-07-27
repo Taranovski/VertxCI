456e7cd34776ff8089ea5077b8516fac75313f61
Taranovski <Taranovski.Alex@gmail.com>
2015-07-27 22:48:25 +0300

    CI 6th try integration test added

---
+++ VertxCIIntegration/src/test/java/vertx/example/vertxci/integration/VertxIntegrationTest.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vertx.example.vertxci.integration;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ *
+ * @author Alyx
+ */
+//@RunWith (SpringJUnit4ClassRunner.class)
+public class VertxIntegrationTest {
+
+    static RestTemplate restTemplate;
+
+    @BeforeClass
+    public static void suiteSetUp() {
+        restTemplate = new RestTemplate();
+    }
+
+    @Before
+    public void setUp() {
+
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testHTTPServer() {
+        String result = restTemplate.getForObject("http://localhost:8081", String.class);
+        assertEquals("Hello World from Vertx (changed 8th time)!", result);
+    }
+
+}
